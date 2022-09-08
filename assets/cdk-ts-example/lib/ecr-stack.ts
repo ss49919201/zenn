104b3765aad05ad2a3f9bae94ab60ad2e77150f3
s-beats <sakashin10291029@gmail.com>
2022-09-08 21:13:42 +0900

    typo

--- assets/cdk-ts-example/lib/ecr-stack.ts
+++ assets/cdk-ts-example/lib/ecr-stack.ts
@@ -1,4 +1,4 @@
-import * as cdk from "aws-cdk-lib/core";
+import * as cdk from "aws-cdk-lib";
 import { Construct } from 'constructs';
 import * as ecr from "aws-cdk-lib/aws-ecr";
 
--- assets/cdk-ts-example/lib/ecs-stack.ts
+++ assets/cdk-ts-example/lib/ecs-stack.ts
@@ -1,4 +1,4 @@
-import * as cdk from "aws-cdk-lib/core";
+import * as cdk from "aws-cdk-lib";
 import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import { Construct } from 'constructs';
