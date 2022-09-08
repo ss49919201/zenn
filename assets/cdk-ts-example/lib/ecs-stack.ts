fee422f7839890f99f84501c2c8c69efc2be5cc6
s-beats <sakashin10291029@gmail.com>
2022-09-08 23:06:37 +0900

    private method

--- assets/cdk-ts-example/lib/ecs-stack.ts
+++ assets/cdk-ts-example/lib/ecs-stack.ts
@@ -8,27 +8,31 @@ type EcsStackProps = cdk.StackProps & {
 };
 
 export class EcsStack extends cdk.Stack {
+    private taskDefinition(arch: ecs.CpuArchitecture): ecs.TaskDefinition {
+        return new ecs.FargateTaskDefinition(this, 'TaskDef', {
+            runtimePlatform: {
+                cpuArchitecture: arch
+            },
+        });
+    }
+
     constructor(scope: Construct, id: string, props?: EcsStackProps) {
         super(scope, id, props);
 
         const cluster = new ecs.Cluster(this, "Cluster");
 
-        const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef', {
-            runtimePlatform: {
-                operatingSystemFamily: ecs.OperatingSystemFamily.LINUX,
-                cpuArchitecture: ecs.CpuArchitecture.ARM64,
-            },
-        });
+        const taskDefinition = this.taskDefinition(ecs.CpuArchitecture.X86_64);
 
-        taskDefinition.addContainer('Arm64', {
+        taskDefinition.addContainer('Container', {
             logging: ecs.LogDriver.awsLogs({ streamPrefix: 'graviton2-on-fargate' }),
             portMappings: [{ containerPort: 80 }],
             image: ecs.ContainerImage.fromEcrRepository(props!.repository, 'server'),
         });
 
-        new ecs.Ec2Service(this, 'Service', {
+        new ecs.FargateService(this, 'Service', {
             cluster,
             taskDefinition,
+            assignPublicIp: true, // ECR のPullが面倒なので、パブリックIPを割り当てる
         });
     }
 }
